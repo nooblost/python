d670ad0df576309e7393e8378c865ba39ad80855
Chris Gee <chrisg123@gmail.com>
2025-04-18 14:19:20 +0800

    sorting

--- 5-3_alien_colors_1.py
+++
@@ -1,9 +0,0 @@
-alien_color = 'green'
-
-if alien_color == 'green':
-    print("You just earned 5 points!")
-
-alien_color = 'red'
-
-if alien_color == 'green':
-    print("You just earned 5 points!")
--- 5-9_no_users.py
+++
@@ -1,9 +0,0 @@
-usernames = ['mike', 'admin', 'chris', 'abbie', 'lee']
-if usernames:
-    for username in usernames:
-        if username == 'admin':
-            print(f"\nWelcome back, {username.title()}, here is the status report.")
-        else:
-            print(f"\nWelcome back, {username.title()}.")
-else:
-    print("We need to get some users!")
