87cec89d2f872a4f7864499f9dc2bf47a8f9766e
Chris Gee <chrisg123@gmail.com>
2025-06-17 12:05:13 +0800

    minor edits

--- chapter 6/aliens.py
+++ chapter 6/aliens.py
@@ -21,7 +21,7 @@ print(alien_0)
 print(f"The alien is {alien_0['color']}.")
 alien_0['color'] = 'yellow'
 print(f"The alien is now {alien_0['color']}.")
-"""
+
 
 alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
 print(f"Original position: {alien_0['x_position']}, current speed: {alien_0['speed']}")
@@ -37,3 +37,10 @@ else:
 
 alien_0['x_position'] = alien_0['x_position'] + x_increment
 print(f"New position: {alien_0['x_position']}")
+"""
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
