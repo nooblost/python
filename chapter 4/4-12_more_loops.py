458f981c12e586c1055fcde0c10437a8b552304e
Chris Gee <chrisg123@gmail.com>
2025-10-01 15:50:39 +0800

    organizational updates

--- chapter 4/4-12_more_loops.py
+++
@@ -1,3 +0,0 @@
-my_foods = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
-
-
--- chapter 5/5-1_conditional_tests.py
+++
@@ -1,6 +0,0 @@
-car = 'subaru'
-print("Is car == 'subaru'? I predict true.")
-print(car == 'subaru')
-
-print("\nIs car == 'audi'? I predict false.")
-print(car == 'audi')
